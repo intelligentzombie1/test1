5a1eb1c93344bb3c357bddbcc2610ce5a9e84be2
intelligentzombie1 <50877510+intelligentzombie1@users.noreply.github.com>
2019-07-03 18:38:41 +0100

    A website i created using bootstrap 4 beta 4

---
+++ custom.js
@@ -0,0 +1,30 @@
+
+    // Back to top button
+    
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+  /*----------------------------
+  Page Scroll
+  ------------------------------ */
+  var page_scroll = $('a.page-scroll');
+  page_scroll.on('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top - 55
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
+
+  
